87e6f6dd1cc35b3f7d281d9c2386a3d9408e8207
Meshari <almeshari93@gmail.com>
2020-04-10 20:49:41 +0300

    Example Conditional Operation

---
+++ src/com/ConditionalOperation.java
@@ -0,0 +1,15 @@
+package com;
+
+public class ConditionalOperation {
+  public static void main(String[] args) {
+    //
+    int februaryDays = 29;
+      String result;
+
+      result = (februaryDays ==28 ) ? "Not a Leap year" : "Leap year";
+    System.out.println(result);
+
+  }
+
+    }
+
