ba06ab3252b88a98103c5a1eb10d3eebec62f467
Meshari <almeshari93@gmail.com>
2020-04-10 19:58:13 +0300

    Example Arthritic Operation

---
+++ src/com/ArithmeticOperation.java
@@ -0,0 +1,28 @@
+package com;
+
+public class ArithmeticOperation {
+  public static void main(String[] args) {
+    //
+  double number1 = 12.5 , number2 = 3.5, result;
+
+  //Using Addition Operation
+      result = number1 + number2;
+    System.out.println("Number1 + Number2 = " + result);
+
+    //Using Subtraction Operation
+      result = number1 - number2;
+    System.out.println("Number1 - Number2 = " + result);
+
+    // Using Multiplication Operation
+      result = number1 * number2;
+    System.out.println("number1 * number2 = " + result);
+
+    //Using division Operation
+      result = number1 / number2;
+    System.out.println("number1 / number2 = " + result);
+
+    // Using Remainder Operation
+    result = number1 % number2;
+    System.out.println("number1 % number2 = " + result);
+  }
+}
