9e060538e13fdf2b12b62f3019e93792d15b0045
Meshari <almeshari93@gmail.com>
2020-04-10 19:46:18 +0300

    Example Assignment Operation

---
+++ src/com/AssignmentOperation.java
@@ -0,0 +1,15 @@
+package com;
+
+public class AssignmentOperation {
+  public static void main(String[] args) {
+    //
+  int number1 , number2;
+  // Assign 5 to number1
+    number1 = 5 ;
+    System.out.println(number1);
+    // Assign value of number1 to number2
+
+      number2 = number1;
+    System.out.println(number2);
+  }
+}
